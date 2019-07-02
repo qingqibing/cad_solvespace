zhuowei/solvespace

Add a very experimental Emscripten port.

src/platform/platform.cpp
@@ -499,6 +499,12 @@ static Platform::Path ResourcePath(const std::string &name) {
     return path;
 }
 
+#elif defined(EMSCRIPTEN)
+
+static Platform::Path ResourcePath(const std::string &name) {
+    return Path::From("res/" + name);
+}
+
 #elif !defined(WIN32)
 
 #    if defined(__linux__)

qingqibing/cad_solvespace

src/platform/platform.cpp
@@ -512,6 +512,12 @@ static Platform::Path ResourcePath(const std::string &name) {
     return path;
 }
 
+#elif defined(__EMSCRIPTEN__)
+
+static Platform::Path ResourcePath(const std::string &name) {
+    return Path::From("res/" + name);
+}
+
 #elif !defined(WIN32)
 
 #    if defined(__linux__)